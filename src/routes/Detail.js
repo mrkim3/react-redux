c866e0088b39ea21352ebfef957cb6e1cfc94a2f
”miri” <Kim.miri333@gmail.com>
2023-04-19 22:05:46 +0900

    u

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -5,21 +5,22 @@ function DetailPage() {
     const navigate = useNavigate();
     const param = useParams();
     const currentState = useSelector((state) => state);
-    //console.log(param, currentState);
+
     const onClickBackBtn = () => navigate(-1);
     const onClickHomeBtn = () => navigate("/");
     const todo = currentState.find((item) => item.id === parseInt(param.id));
     const others = currentState.filter((todo) => todo.id !== parseInt(param.id));
-    //console.log(todo);
+
     return (
         <>
-            <h1>Detail of {todo.text}</h1>
-            <b>자세한 설명은 생략한다.</b>
-            <h2>Others</h2>
+            <h1>{todo.text}</h1>
+            <h5>Create at: {todo.id}</h5>
+
             <ul>
-                {others.map((todo) => (
-                    <Link key={todo.id} to={`/${todo.id}`}>
-                        <li>{todo.text}</li>
+
+                {others.map((r) => (
+                    <Link key={r.id} to={`/${r.id}`}>
+                        <li>{r.text}</li>
                     </Link>
                 ))}
             </ul>
